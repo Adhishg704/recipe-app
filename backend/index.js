8226769b5e598d8889c0595adcdcf05aceb7c76a
AD <adhish.g704@gmail.com>
2023-11-22 10:30:36 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT;
 app.use(express.json());
 
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://recipe-app-ten-rho.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type"],
     credentials: true
