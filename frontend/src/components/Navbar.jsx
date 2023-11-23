4490b788c3454b742f69ea930a341f6073904e30
AD <adhish.g704@gmail.com>
2023-11-23 10:38:07 +0530

    updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useCookies } from 'react-cookie';
 import Sidebar from './Sidebar';
-import { faHome, faSearch, faCog, faInfo } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faCog, faInfo } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
 
